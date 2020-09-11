9839c92a89e1a7bd26071bcb068512e780b4680a
Martin <7342117+MartinNikolovMarinov@users.noreply.github.com>
2020-09-11 12:07:47 +0300

    Avoid Sep allocation in Write
    Co-authored-by: Martin Marinov <martin.marinov@acronis.com>

--- wrapper_test.go
+++ wrapper_test.go
@@ -2,38 +2,39 @@ package textwrapper_test
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 
 	"github.com/emersion/go-textwrapper"
 )
 
 func TestNew(t *testing.T) {
-	tests := []struct{
-		input []string
+	tests := []struct {
+		input    []string
 		expected string
 	}{
 		{
-			input: []string{"helloworldhelloworldhelloworld"},
+			input:    []string{"helloworldhelloworldhelloworld"},
 			expected: "hello/world/hello/world/hello/world",
 		},
 		{
-			input: []string{"helloworldhelloworldhe"},
+			input:    []string{"helloworldhelloworldhe"},
 			expected: "hello/world/hello/world/he",
 		},
 		{
-			input: []string{"helloworldhelloworldhe", "ll"},
+			input:    []string{"helloworldhelloworldhe", "ll"},
 			expected: "hello/world/hello/world/hell",
 		},
 		{
-			input: []string{"helloworldhelloworldhe", "llo"},
+			input:    []string{"helloworldhelloworldhe", "llo"},
 			expected: "hello/world/hello/world/hello",
 		},
 		{
-			input: []string{"helloworldhelloworldhe", "lloworld"},
+			input:    []string{"helloworldhelloworldhe", "lloworld"},
 			expected: "hello/world/hello/world/hello/world",
 		},
 		{
-			input: []string{"helloworldhelloworldhe", "llo", "wor", "ld"},
+			input:    []string{"helloworldhelloworldhe", "llo", "wor", "ld"},
 			expected: "hello/world/hello/world/hello/world",
 		},
 	}
@@ -49,6 +50,45 @@ func TestNew(t *testing.T) {
 		output := b.String()
 		if output != test.expected {
 			t.Error("Got " + output + " instead of " + test.expected)
+		}
+	}
+}
+
+func BenchmarkWriteWith10K(b *testing.B) {
+	b.ReportAllocs()
+
+	input := make([]byte, 10_000)
+	w := textwrapper.New(ioutil.Discard, "/", 3)
+	for i := 0; i < b.N; i++ {
+		_, err := w.Write(input)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkWriteWith100K(b *testing.B) {
+	b.ReportAllocs()
+
+	input := make([]byte, 100_000)
+	w := textwrapper.New(ioutil.Discard, "/", 3)
+	for i := 0; i < b.N; i++ {
+		_, err := w.Write(input)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkWriteWith1M(b *testing.B) {
+	b.ReportAllocs()
+
+	input := make([]byte, 1_000_000)
+	w := textwrapper.New(ioutil.Discard, "/", 3)
+	for i := 0; i < b.N; i++ {
+		_, err := w.Write(input)
+		if err != nil {
+			b.Error(err)
 		}
 	}
 }
